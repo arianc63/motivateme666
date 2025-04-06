a7588b36377d1e5d03a5403d052a88445867744b
arianc63 <recoveryarian2018@gmail.com>
2025-04-06 18:30:40 +0330

    add index.js and package.json for tea

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const messages = [
-  "You got this!",
-  "Keep going!",
-  "Every day is a new chance!",
-  "You're stronger than you think!",
-  "Push your limits!"
-];
-
-const randomIndex = Math.floor(Math.random() * messages.length);
-console.log(messages[randomIndex]);
+console.log("Stay motivated, you got this! 💪")
