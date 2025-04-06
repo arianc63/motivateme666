10d84bcc4d6926db14d840a1696a2be7afbb8027
arianc63 <recoveryarian2018@gmail.com>
2025-04-06 18:18:11 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const messages = [
+  "You got this!",
+  "Keep going!",
+  "Every day is a new chance!",
+  "You're stronger than you think!",
+  "Push your limits!"
+];
+
+const randomIndex = Math.floor(Math.random() * messages.length);
+console.log(messages[randomIndex]);
